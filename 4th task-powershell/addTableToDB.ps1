0ac99b19c33fb5c145a02e428f3dbb798f0d5d87
marktattwo <mark-tattwo@hotmail.com>
2015-06-05 19:16:17 +0700

    can replace name and execute sql file but have problem with duplicate key

---
+++ 4th task-powershell/addTableToDB.ps1
@@ -0,0 +1,6 @@
+﻿$a = Get-Content -Encoding UTF8 C:\temp\product_bundle_tab.sql 
+$d= Get-Date
+$filename = "product_bundle_tab" + "_" + $d.Year.ToString() + "_" + $d.Month.ToString() + "_" + $d.Day.ToString()
+$a = $a.Replace("product_bundle_tab" , $filename)
+Set-Content -Encoding UTF8 -Path C:\temp\$filename.sql -Value $a
+&cmd /c "C:\xampp\mysql\bin\mysql.exe --default-character-set=utf8 -h 172.16.6.200 -u root -P 3306 test < C:\temp\$filename.sql"
